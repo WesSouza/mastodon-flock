1b442232aafba53117d744516759640f612c4e40
Wes Souza <noreply@wes.dev>
2022-11-27 16:49:12 -0500

    Welcome wizard step

--- src/components/Wizard/Welcome.tsx
+++ src/components/Wizard/Welcome.tsx
@@ -1,21 +1,28 @@
-import { Button } from "react95";
-
+import { Paragraph } from "../React95/Paragraph";
 import { WizardWindow } from "./WizardWindow";
 
-export function Welcome({ goNext }: { goNext: () => void }) {
+export function Welcome({
+  cancel,
+  goNext,
+}: { cancel: () => void; goNext: () => void }) {
   return (
     <WizardWindow
-      footer={
-        <Button primary={true} onClick={goNext}>
-          Connect Twitter &gt;
-        </Button>
-      }
-      imageAlt="A pixel art drawing of an old school computer and CRT monitor, by its left a set of a yellow old school phone on top of a modem. In front of it, a folded piece of paper with a yellow pencil on top."
+      cancelAction={{ label: "Cancel", onClick: cancel }}
+      imageAlt="A drawing of an old computer besides a phone and a modem in front of a piece of paper and pencil."
       imageSrc="/images/setup.png"
-      onClose={() => {}}
+      nextAction={{ label: "Next >", onClick: goNext }}
+      onClose={cancel}
       title="Welcome"
     >
-      Welcome
+      <Paragraph>Welcome to the Mastodon Flock installation wizard.</Paragraph>
+      <Paragraph>
+        This program will guide you through finding your Twitter contacts on the
+        Fediverse, provided they have added their external contact information
+        on their profile name, description, URL or pinned Tweet.
+      </Paragraph>
+      <Paragraph>
+        Click Next to authorize this program to read your Twitter information.
+      </Paragraph>
     </WizardWindow>
   );
 }
