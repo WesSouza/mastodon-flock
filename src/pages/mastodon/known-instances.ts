cab2e36657485006786e655a2b838723a3279d85
Wes Souza <noreply@wes.dev>
2022-12-02 18:19:30 -0500

    Return only Mastodon instances with more than 50 users by default

--- src/pages/mastodon/known-instances.ts
+++ src/pages/mastodon/known-instances.ts
@@ -1,15 +1,20 @@
 import type { APIRoute } from "astro";
 import mongoose from "mongoose";
 
-import { FederatedInstance } from "../../models/FederatedInstance";
+import {
+  FederatedInstance,
+  IFederatedInstance,
+} from "../../models/FederatedInstance";
 import type { MastodonInstance } from "../../types";
 import { responseJsonError } from "../../utils/http-response";
 
 export const get: APIRoute = async function get(context) {
-  const { request } = context;
+  const { request, url } = context;
   if (!request.headers.get("accept")?.startsWith("application/json")) {
     return responseJsonError(400, "badRequest");
   }
+
+  const allInstances = Boolean(url.searchParams.get("allInstances"));
 
   try {
     await mongoose.connect(import.meta.env.MONGODB_URI);
@@ -18,9 +23,13 @@ export const get: APIRoute = async function get(context) {
     return responseJsonError(500, "databaseConnectionError");
   }
 
+  const query: mongoose.FilterQuery<IFederatedInstance> = allInstances
+    ? {}
+    : { "usage.usersTotal": { $gte: 50 } };
+
   // TODO: Paginate once the know Fediverse gets out of hand
   const federatedInstances: MastodonInstance[] = await FederatedInstance.find(
-    {},
+    query,
     {
       _id: 0,
       instanceUrl: 1,
